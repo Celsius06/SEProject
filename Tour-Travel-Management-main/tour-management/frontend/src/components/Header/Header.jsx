85d5b4a4f0cf6badfc54c2efba1012d9e9b664df
binhbb2204 <binhbb2204@gmail.com>
2024-11-02 00:56:03 +0700

    home page

--- Tour-Travel-Management-main/tour-management/frontend/src/components/Header/Header.jsx
+++ Tour-Travel-Management-main/tour-management/frontend/src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Container, Row, Button} from 'reactstrap'
-import { NavLink, Link} from 'react-router-dom';
-import logo from './../../assets/images/logo.png'
-import gifImage from './../../assets/images/TAB.gif'
+import {NavLink, Link} from 'react-router-dom';
+//import logo from './../../assets/images/logo.png'
+import logo from './../../assets/images/TAB.gif'
+import './header.css'
 const nav_links = [
   {
     path: '/home',
@@ -23,11 +24,34 @@ const Header = () => {
       <Container>
         <Row>
           <div className="nav__wrapper d-flex align-items-center justify-content-between">
-            {/* <div className="logo">
+            {/*===============Logo Start====================*/}
+             <div className="logo">
               <img src = {logo} alt = ""/>
-            </div> */}
-            <div className="gif-container">
-              <img src={gifImage} alt="Animated GIF"/> {/* Add the GIF here */}
+            </div> 
+            
+            {/*===============Logo End====================*/}
+            {/*===============Menu Start====================*/}
+            <div className= "navigation">
+              <ul className= "menu d-flex align-items-center gap-5">
+                {
+                  nav_links.map((item, index)=>(
+                    <li className="nav__item" key = {index}>
+                      <NavLink to={item.path}>{item.display}</NavLink>
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+            {/*===============Menu End====================*/}
+            <div className="nav__right d-flex align-items-center gap-4 ">
+              <div className="nav__btns d-flex align-items-center gap-4 ">
+                <Button className = "btn secondary__btn"><Link to='/login'>Login</Link></Button>
+                <Button className = "btn primary__btn"><Link to='/register'>Register</Link></Button>
+
+              </div>
+              <span className="mobile_menu">
+              <i class="ri-menu-line"></i>
+              </span>
             </div>
           </div>
         </Row>
